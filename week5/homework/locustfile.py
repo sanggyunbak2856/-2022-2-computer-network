ad56fa7efe8c533225a778167037a9edabde4704
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-09-30 11:43:48 +0900

    homework

---
+++ week5/homework/locustfile.py
@@ -0,0 +1,15 @@
+from locust import HttpUser, task
+
+class User(HttpUser):
+    
+    wait_time = between(1, 5)
+    
+    @task
+    def get_index(self):
+        self.client.get('/')
+        self.client.get('/index.html')
+        self.client.get('/201702011.html')
+        self.client.get('/notfound.html')
+        self.client.get('/mystyle.css')
+        self.client.get('/myscript.js')
+        self.client.get('/myimage.jpg')
